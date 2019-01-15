3eb3fee0d7d069dde5a085193405c0fe7abc244c
Murat Bilgel <bilgelm@gmail.com>
2019-01-15 11:42:09 -0500

    Update pages - 2019-01-15 11:42:04

--- update_pages.sh
+++ update_pages.sh
@@ -8,11 +8,11 @@ while true; do
 TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S'`
 
 # Clean out .ipynb_checkpoints in folder structure
-rm -rf `find . -name ".ipynb_checkpoints"`
+rm -rf `find -name ".ipynb_checkpoints"`
 
 # Reset the counts of all notebook cells
 find . -type f -name "*.ipynb" \
-    -exec sed -i '' 's/execution_count\b.*$/execution_count": null,/' {} +
+    -exec sed -i 's/execution_count\b.*$/execution_count": null,/' {} +
 
 # Convert the notebooks to HTML
 jupyter nbconvert --to html --template full index.ipynb --output-dir=docs
@@ -20,17 +20,18 @@ jupyter nbconvert --to html --template full notebooks/*ipynb \
     --output-dir=docs/notebooks/
 
 # Replace all .ipynb-links with .html-liks
-find docs -type f -name "*.html" -exec sed -i '' 's/ipynb\&/html\&/g' {} +
-find docs -type f -name "*.html" -exec sed -i '' 's/ipynb#/html#/g' {} +
-find docs -type f -name "*.html" -exec sed -i '' 's/ipynb\"/html\"/g' {} +
+find docs -type f -name "*.html" -exec sed -i 's/ipynb\&/html\&/g' {} +
+find docs -type f -name "*.html" -exec sed -i 's/ipynb#/html#/g' {} +
+find docs -type f -name "*.html" -exec sed -i 's/ipynb\"/html\"/g' {} +
 
 # Delete the Button "Show HTML code" from index.html
-sed -i '' '/Show HTML code/d' docs/index.html
+sed -i '/Show HTML code/d' docs/index.html
 
 # Add Google Analytics script to each homepage
 for h in `find docs -maxdepth 2 -name "*html"`
 do
-    sed -e '/<\/head>/ {r static/template_google_analytics.rst' -e 'd}' $h > tmp.rst
+    sed '/<\/head>/ {r static/template_google_analytics.rst
+    d}' $h > tmp.rst
 
     mv tmp.rst $h
 done
@@ -38,7 +39,7 @@ done
 # Add Footer to all html-notebooks
 for h in `find docs -maxdepth 2 -name "*html"`
 do
-    sed -i '' 's/<\/body>/<\/body><div class="h3" style="right:0;bottom:0;left:0;padding:1rem;text-align:center;"><p style="white-space:pre"><a href="https:\/\/miykael.github.io\/nipype_tutorial\/">Home<\/a>\&emsp;|\&emsp;<a href="https:\/\/github.com\/miykael\/nipype_tutorial">github<\/a>\&emsp;|\&emsp;<a href="http:\/\/nipype.readthedocs.io">Nipype<\/a><\/p><\/div>/' $h
+    sed -i 's/<\/body>/<\/body><div class="h3" style="right:0;bottom:0;left:0;padding:1rem;text-align:center;"><p style="white-space:pre"><a href="https:\/\/miykael.github.io\/nipype_tutorial\/">Home<\/a>\&emsp;|\&emsp;<a href="https:\/\/github.com\/miykael\/nipype_tutorial">github<\/a>\&emsp;|\&emsp;<a href="http:\/\/nipype.readthedocs.io">Nipype<\/a><\/p><\/div>/' $h
 
 done
 
